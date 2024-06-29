a624b821557c1a396449b01ab52df44943a759c1
DiamondPG <111537024+DiamondPG@users.noreply.github.com>
2024-06-28 22:05:06 -0500

    Initial Commit

---
+++ DiplomacyOrders/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using DiplomacyOrders.assets.screens;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiplomacyOrders
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Home home = new Home();
+        Map map = new Map();
+        SubmitOrders orders = new SubmitOrders();
+        public MainWindow()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Content.Content = home;
+        }
+
+        private void CloseBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeBTN_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Border_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void HomeBTN_Click(object sender, EventArgs e)
+        {
+            Content.Content = home;
+        }
+        private void ViewMapBTN_Click(object sender, EventArgs e)
+        {
+            Content.Content = map;
+        }
+        private void OrdersBTN_Click(object sender, EventArgs e)
+        {
+            Content.Content = orders;
+        }
+    }
+}
---
+++ DiplomacyOrders/assets/screens/SubmitOrders.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiplomacyOrders.assets.screens
+{
+    /// <summary>
+    /// Interaction logic for SubmitOrders.xaml
+    /// </summary>
+    public partial class SubmitOrders : UserControl
+    {
+        public SubmitOrders()
+        {
+            InitializeComponent();
+        }
+
+        private string GetLastRow()
+        {
+            string[] rows = OrdersTXT.Text.Split("\n");
+            return rows[rows.Length - 1];
+        }
+
+        private void CorrectOrders(string lastRow, string? input)
+        {
+            string[] sections = OrdersTXT.Text.Split(" ");
+            
+        }
+
+        private void OrdersTXT_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key != null)
+            {
+                switch (e.Key)
+                {
+                    case Key.A:
+                        CorrectOrders(GetLastRow(), "A");
+                        break;
+                    case Key.F:
+                        CorrectOrders(GetLastRow(), "F");
+                        break;
+                    case Key.Space:
+                        CorrectOrders(GetLastRow(), "Space");
+                        break;
+                }
+            }
+        }
+    }
+}
