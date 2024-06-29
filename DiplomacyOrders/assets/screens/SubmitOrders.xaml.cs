70940b60f8e9fb615162054f31952c4e8d649a4e
DiamondPG <111537024+DiamondPG@users.noreply.github.com>
2024-06-29 01:07:16 -0500

    Added Order Auto-Complete

--- DiplomacyOrders/assets/screens/SubmitOrders.xaml.cs
+++ DiplomacyOrders/assets/screens/SubmitOrders.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace DiplomacyOrders.assets.screens
 {
     /// <summary>
@@ -20,9 +24,11 @@ namespace DiplomacyOrders.assets.screens
     /// </summary>
     public partial class SubmitOrders : UserControl
     {
+        Dictionary<string, List<string>> acronyms = new Dictionary<string, List<string>>();
         public SubmitOrders()
         {
             InitializeComponent();
+            acronyms = JsonConvert.DeserializeObject<Dictionary<string, List<string>>>(File.ReadAllText("./assets/data/countries.json"));
         }
 
         private string GetLastRow()
@@ -30,10 +36,64 @@ namespace DiplomacyOrders.assets.screens
             string[] rows = OrdersTXT.Text.Split("\n");
             return rows[rows.Length - 1];
         }
-
-        private void CorrectOrders(string lastRow, string? input)
+        bool armyFleetDeclaration = false;
+        private void CorrectOrders(string lastRow, string input)
         {
             string[] sections = OrdersTXT.Text.Split(" ");
+            string entry = sections.Last();
+            Trace.WriteLine(entry);
+            switch (input)
+            {
+                case "Space":
+                    if(!armyFleetDeclaration)
+                    {
+                        foreach (var entree in acronyms)
+                        {
+                            if (entree.Value.Contains(entry, StringComparer.OrdinalIgnoreCase))
+                            {
+                                Trace.WriteLine($"Found '{entry}' under acronym: {entree.Key}");
+                                entry = entree.Key;
+                                break;
+                            }
+                        }
+                    }
+                    if(entry.ToLower() == "s" || entry.ToLower() == "sup")
+                    {
+                        entry = "Support";
+                    }
+                    if (entry.ToLower() == "a")
+                    {
+                        entry = "Army";
+                    }
+                    if (entry.ToLower() == "f")
+                    {
+                        entry = "Fleet";
+                    }
+                    if (entry.ToLower() == "m" || entry.ToLower() == "move")
+                    {
+                        entry = " - ";
+                    }
+                    if(entry.ToLower() == "c")
+                    {
+                        entry = "Convoy";
+                    }
+                    if (entry.ToLower() == "v")
+                    {
+                        entry = "Via";
+                    }
+                    break;
+                case "Enter":
+                    entry = "\n";
+                    break;
+            }
+            sections[sections.Length - 1] = entry;
+            string text = "";
+            foreach(string section in sections)
+            {
+                text += section + " ";
+            }
+            OrdersTXT.Text = text;
+            OrdersTXT.CaretIndex = text.Length;
             
         }
 
@@ -43,14 +103,12 @@ namespace DiplomacyOrders.assets.screens
             {
                 switch (e.Key)
                 {
-                    case Key.A:
-                        CorrectOrders(GetLastRow(), "A");
-                        break;
-                    case Key.F:
-                        CorrectOrders(GetLastRow(), "F");
-                        break;
+                    
                     case Key.Space:
                         CorrectOrders(GetLastRow(), "Space");
+                        break;
+                    case Key.Enter:
+                        CorrectOrders(GetLastRow(), "Enter");
                         break;
                 }
             }
